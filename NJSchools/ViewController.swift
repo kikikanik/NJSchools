2675020c3f888f3946505d741476ed2c56a08f81
Kiki Kanik <kiki.kanik@gmail.com>
2021-10-04 18:48:57 -0400

    Initial Commit

---
+++ NJSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NJSchools
+//
+//  Created by Kinneret Kanik on 04/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
